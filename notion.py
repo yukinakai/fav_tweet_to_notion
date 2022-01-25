c387287dcf93056b43462425d280d1bc032f2a82
yukinakai <10libra20.y@gmail.com>
2022-01-25 18:50:42 +0900

    check duplicated tweet on notion #13 #8

--- notion.py
+++ notion.py
@@ -6,10 +6,7 @@ import sys
 from models import notion_params
 
 notion_page_prams = notion_params.NotionPage()
-
-def create_url():
-    url = "https://api.notion.com/v1/pages"
-    return url
+NOTION_DATABASE_ID = config.NOTION_DATABASE_ID
 
 def create_header():
     NOTION_API_KEY = config.NOTION_API_KEY
@@ -21,9 +18,8 @@ def create_header():
     return headers
 
 def format_data(data):
-    database_id = config.NOTION_DATABASE_ID
     body_params = notion_page_prams.page(data)
-    body_params['parent'] = {'database_id': database_id}
+    body_params['parent'] = {'database_id': NOTION_DATABASE_ID}
 
     children = list()
     if 'attached_media_url' in data:
@@ -50,13 +46,36 @@ def connect_to_endpoint(url, headers, data):
         )
     return response.json()
 
+def get_tweets_in_notion(headers):
+    url = "https://api.notion.com/v1/databases/{}/query".format(NOTION_DATABASE_ID)
+    payload = {"page_size": 5}
+    has_more = True
+    tweets_in_notion = list()
+    while has_more:
+        json_response = connect_to_endpoint(url, headers, payload)
+        for result in json_response['results']:
+            tweets_in_notion.append(result['properties']['Tweet_id']['rich_text'][0]['plain_text'])
+        has_more = json_response['has_more']
+        payload['start_cursor'] = json_response['next_cursor']
+    tweets_in_notion = list(set(tweets_in_notion))
+    return tweets_in_notion
+
+def create_page_on_notion(headers, data):
+    url = "https://api.notion.com/v1/pages"
+    body_params = format_data(data)
+    json_response = connect_to_endpoint(url, headers, body_params)
+    return json_response
+
 def main():
-    url = create_url()
     headers = create_header()
+    # get tweet_id in notion
+    tweets_in_notion = get_tweets_in_notion(headers)
+    # create page on notion
     data = twitter.main()
     for datum in data:
-        body_params = format_data(datum)
-        json_response = connect_to_endpoint(url, headers, body_params)
+        if datum['tweet_id'] in tweets_in_notion:
+            continue
+        json_response = create_page_on_notion(headers, datum)
         print(json.dumps(json_response, indent=4, sort_keys=True))
 
 
